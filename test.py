8f963c98703c2a1b473df52a788e2d578375bb51
alexm <alex-m@outlook.com>
2023-01-11 22:03:13 -0800

    Comment out unused imports, for now

--- test.py
+++ test.py
@@ -3,8 +3,8 @@
 
 from unittest import TestCase
 from app import app
-from flask import session
-from boggle import Boggle
+# from flask import session
+# from boggle import Boggle
 
 app.config['TESTING'] = True
 app.config['DEBUG_TB_HOSTS'] = ['dont-show-debug-toolbar']
