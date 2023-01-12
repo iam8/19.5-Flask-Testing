d29b132114512938781d220528f3b4e4ed8c137e
alexm <alex-m@outlook.com>
2023-01-11 17:00:00 -0800

    Add header and module docstring; setup Flask app and debug toolbar

--- app.py
+++ app.py
@@ -1,3 +1,19 @@
+# Ioana A Mititean
+# Exercise 19.5 - Flask Testing (Boggle)
+
+"""
+Main code for Boggle application - Flask setup, routes, and view functions.
+"""
+
+from flask import Flask, request, render_template, redirect, flash, session
+from flask_debugtoolbar import DebugToolbarExtension
+
 from boggle import Boggle
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "ceva foarte secreta"
+# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+debug = DebugToolbarExtension(app)
 
 boggle_game = Boggle()
