37ba1ea9305a80a4b85a8cbdc17bead561525c6a
alexm <alex-m@outlook.com>
2023-01-12 14:59:41 -0800

    Add comments to functions; remove console.log statement; add jQuery object for validity message on homepage; add function solely to retrieve relevant response data; add code to guess form event handler to display validity message on homepage

--- static/app.js
+++ static/app.js
@@ -6,7 +6,10 @@
 
 const $guessForm = $("#guess-form");
 const $wordInput = $("#word-input");
+const $validityMsg = $("#validity-msg")
 
+
+// Submit the word guess to the server at '/process_guess' and return the server response
 async function submitGuess(word) {
     const response = await axios.post(
         "/process_guess",
@@ -15,13 +18,26 @@ async function submitGuess(word) {
         }
     );
 
-    console.log(response);
+    return response;
 }
 
+
+// Get the validity of the word from the server response data
+function getWordValidity(response) {
+    return response.data["result"];
+}
+
+
 // Upon submission of word guess form, send the guess to the server (via Axios)
+// Update the app homepage with a message declaring the validity of the word guess (ok, not on
+// board, not word)
 $guessForm.on("submit", async function (evt) {
     evt.preventDefault();
     const word_guess = $wordInput.val();
+    $(this).trigger("reset");
 
-    await submitGuess(word_guess);
+    const response = await submitGuess(word_guess);
+    const validity = getWordValidity(response);
+    $validityMsg.text(`The word is ${validity}`);
+
 })
