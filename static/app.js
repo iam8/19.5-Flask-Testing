589c8efa0fd6482cac507c1b01b698409749a25a
alexm <alex-m@outlook.com>
2023-01-11 21:30:10 -0800

    Retrieve user guess from input and send it as post request via Axios to Boggle homepage (temporarily)

--- static/app.js
+++ static/app.js
@@ -5,20 +5,27 @@
 "use strict";
 
 const $guessForm = $("#guess-form");
+const $wordInput = $("#word-input");
 
 async function submitGuess(word) {
     const response = await axios.post(
-        "/submit_guess",
+        "/",
         {
-            guess: word
+            "guess": word
         }
-    )
+    );
+
+    console.log(response)
 }
 
 // Upon submission of word guess form, send the guess to the server (via Axios)
 $guessForm.on("submit", async function (evt) {
     evt.preventDefault();
-    // Call function to do axios post request here
+    console.log("Submitted guess form");
+    const word_guess = $wordInput.val();
+    console.log(word_guess);
 
+    // Call function to do axios post request here
+    await submitGuess(word_guess);
 })
 
