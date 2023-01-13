03baae07292279756b5c9aff85533ffc1051ba31
alexm <alex-m@outlook.com>
2023-01-13 14:20:34 -0800

    Add and use function to calculate score for a word; rename a variable

--- static/app.js
+++ static/app.js
@@ -41,15 +41,30 @@ function getValidityMsg(response) {
 }
 
 
+// Determine the score for a word guess with the given validity message
+function calculateScore(word, validityMsg) {
+    if (validityMsg === "The word is valid!") {
+        return word.length;
+    }
+
+    return 0;
+}
+
+
 // Upon submission of word guess form, send the guess to the server (via Axios)
 // Update the app homepage with a message declaring the validity of the word guess (ok, not on
 // board, not word)
 $guessForm.on("submit", async function (evt) {
     evt.preventDefault();
-    const word_guess = $wordInput.val();
+    const wordGuess = $wordInput.val();
     $(this).trigger("reset");
 
-    const response = await submitGuess(word_guess);
+    const response = await submitGuess(wordGuess);
     const validity = getValidityMsg(response);
     $validityMsg.text(validity);
+
+    // Update total score
+    scoreTotal += calculateScore(wordGuess, validity);
+
+    // TODO: Display total score on main page
 })
