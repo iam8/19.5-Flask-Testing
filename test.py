96a45cf17bc6a6685e687e38921af35f4359a3ed
alexm <alex-m@outlook.com>
2023-01-14 19:03:02 -0800

    Add docstrings and tests for initial session and submitting guess

--- test.py
+++ test.py
@@ -15,11 +15,48 @@ class FlaskTests(TestCase):
     Tests for every view and helper function in Boggle Flask application (app.py).
     """
 
-    def test_homepage(self):
-        pass
+    def test_homepage_get(self):
+        """
+        Test that GET "/" results in a code of 200 (OK) and contains the form to submit a guess.
+        """
 
-    def test_process_guess(self):
-        pass
+        with app.test_client() as client:
+            resp = client.get("/")
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn("<h2>Guess a word!</h2>", html)
+
+    def test_initial_session(self):
+        """
+        Test that for a first-time user, the session is initialized with the correct info.
+        """
+
+        with app.test_client() as client:
+            client.get("/")
+            board = session.get("board")
+
+            self.assertIsInstance(board, list)
+            self.assertFalse("max" in session)
+            self.assertFalse("num_games" in session)
+
+    def test_guess_submit(self):
+        """
+        Test submission of word guess:
+        - Status code of 200 (OK)
+        - Server response data contains correct key and a message
+        """
+
+        with app.test_client() as client:
+
+            with client.session_transaction() as change_session:
+                change_session['board'] = Boggle().make_board()
+
+            resp = client.post("/process_guess",
+                               json={"guess": "word"})
+            data = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
 
     def test_update_stats(self):
         pass
